c3f7f220f2eeab49aa502107d539227bff5980d6
kozurasergj <kozura.sergj33@gmail.com>
2023-04-02 10:41:09 +0300

    delete component Htag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { Htag } from '@/components';
+// import { Htag } from '@/components';
 import { Button } from '@/components';
-
+import styles from '../styles/Home.module.css';
 export default function Home(): JSX.Element {
   return (
     <div>
-      <Htag tag='h1'>Text</Htag>
+      <h1 className={styles.h1__header}>Hello1!</h1>
+      <h2 className={styles.h2__header}>Hello2!</h2>
+      <h3 className={styles.h3__header}>Hello3!</h3>
       <Button apperance='primary'>Кнопка</Button>
       <Button apperance='ghost'>Кнопка</Button>
     </div>
