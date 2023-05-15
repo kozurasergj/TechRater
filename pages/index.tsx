e079a6916df42745756c4798c1744cbbafcf53df
kozurasergj <kozura.sergj33@gmail.com>
2023-05-15 12:33:00 +0300

    added photo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,15 @@ import { withLayout } from '@/layout/Layout';
 import axios from 'axios';
 import { GetStaticProps } from 'next';
 import React, { useState } from 'react';
+import Image from 'next/image';
 
 const Home = ({ menu }: HomeProps): JSX.Element => {
   const [rating, setRating] = useState<number>(4);
   return (
     <>
-      <Htag tag='h1'>Welcome to our website!✋😀 </Htag>
+      <h1>Welcome to our website!✋😀</h1>
       <p>Here you will find the most truthful information about our courses. We offer a wide selection of courses on various topics at very affordable prices. On each course page, you will be able to see its price and reviews from our real students. We guarantee the quality and usefulness of our courses, so we confidently recommend you to start your learning journey today!</p>
+      <Image src="/bg.jpg" width={900} height={700} alt="Welcome to our website" style={{ width: '100%' }} />
     </>
   );
 };
